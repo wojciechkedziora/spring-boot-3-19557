8173b9abce6729d1d98ce9fd9cb28c1006b58a74
w.kedziora <w.kedziora@ocado.com>
2023-11-06 11:44:43 +0100

    Spring boot upgrade to 3.1.5 + other minor updates

--- src/main/java/com/example/demo/MetricsConfig.java
+++ src/main/java/com/example/demo/MetricsConfig.java
@@ -1,16 +1,17 @@
 package com.example.demo;
 
 import io.micrometer.core.instrument.Gauge;
-import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MetricsConfig {
+
     @Bean
-    public Gauge queueSize(MeterRegistry registry, QueueItemRepository repository) {
-
-        return Gauge.builder("queueSize", repository::count).register(registry);
+    public MeterBinder queueMeterBinder(QueueItemRepository repository) {
+        return (registry) -> {
+            Gauge.builder("queueSize", repository::count).register(registry);
+        };
     }
-
 }
